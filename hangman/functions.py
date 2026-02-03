4fb2b30058747b1f4b5b07e1b1e99489f0db0fd7
Joshua <167120812+Kointaker@users.noreply.github.com>
2026-02-03 08:14:19 -0500

    Implement guess collection and placeholder for check function
    Added functions for collecting guesses and checking them.

--- hangman/functions.py
+++ hangman/functions.py
@@ -1,5 +1,8 @@
 # Functions
 
+import random
+import time
+
 
 def display_figure(bad_guesses):
     hangmen = ["""
@@ -48,6 +51,38 @@ def display_figure(bad_guesses):
     
     print(hangmen[bad_guesses])
     return
+
+
+
+# Functions: Continued
+
+# # Variables/to start:
+# starter_list
+# choice: Word in normal format
+# word 
+# bad_guesses 
+
+def collect_guess():
+    global guess
+    while True:
+        guess = input("Enter a letter for your guess: ").lower()
+        
+        if len(guess) == 1:
+            break
+        else:
+            print("Hey! Only one letter!")
+        
+
+
+def check(guess):
+    pass
+
+
+
+
+
+
+
 
 
 
