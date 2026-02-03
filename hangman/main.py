69c3486e6bc5423cdef34004bcd6ac7707cf1c15
Joshua <167120812+Kointaker@users.noreply.github.com>
2026-02-03 08:14:37 -0500

    Refactor hangman game structure and imports
    Refactored the hangman game code by removing the display_figure function and organizing imports.

--- hangman/main.py
+++ hangman/main.py
@@ -6,64 +6,32 @@
 # -If user loses, show game over screen, show them the word, and allow them to try again
 # -If user wins, show win screen, and allow them to try againimport time
 
+import random
+import time
+# From other file
+from functions import display_figure, collect_guess
+
+
+# Variables/to start:
+starter_list = ["hello", "goodbye", "good", "bad", "happy", "sad", "content"]
+word = []
+choice = starter_list[random.randint(0, 6)]
+for i in list(choice):
+    word.append(i)
+print(word)
+bad_guesses = 0
 
 
 
 
 
-
-
-
-def display_figure(bad_guesses):
-    hangmen = ["""
-+------+
-|
-|
-|
-|
-=========""","""
-+------+
-|      o
-|
-|
-|
-=========""","""
-+------+
-|      o
-|      |
-|
-|
-=========""","""
-+------+
-|      o
-|     /|
-|
-|
-=========""","""
-+------+
-|      o
-|     /|\\
-|
-|
-=========""","""
-+------+
-|      o
-|     /|\\
-|     /
-|
-=========""","""
-+------+
-|      o
-|     /|\\
-|     / \\
-|
-========="""]
-    
-    print(hangmen[bad_guesses])
-    return
+def main():
+    print("Welcome to hangman! You will be allowed to guess incorectly 6 times, after that, you lose!")
+    time.sleep(1)
+    collect_guess()
 
 
 
 
-
-
+if __name__ == "__main__":
+    main()
