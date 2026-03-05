a1eef210b9ef242f4abb55ac9dfa639cacc62d46
Joshua Ferreira <kointaker@Joshuas-MacBook-Air-129.local>
2026-03-04 19:31:16 -0500

    Added starter code for assignment

---
+++ contact_list/main.py
@@ -0,0 +1,56 @@
+# Contact List Starter Code
+# Your name, the date
+
+### import satements
+
+### functions
+
+## add contact - adds a new key, value dictionary
+# to the contacts dictionary
+# parameters: a dictionary
+# return: nothing
+def add_contact():
+    # get new contact_info
+    # add new key, value to contacts dictionary
+    pass
+
+## delete contact - deletes contact from dictionary
+# parameters: a dictionary
+# return: nothing
+def remove_contact(dict):
+    # test if nickname is in dictionary
+    # if name is found, remove key, value
+    # else, tell user contact is not found
+    pass
+
+## show contact info
+# print single contact from dictionary
+# parameters: a dictionary
+# return: nothing
+def show_contact_info():
+    # if name in dictionary,
+    # print information about contact
+    pass
+
+## show contact phone number
+# print phone number for single contact
+# parameters: a dictionary
+# return: nothing
+def show_phone_number():
+    # if name in dictionary
+    # print phone number of contact
+    pass
+
+
+### main program
+
+## variables
+
+# contact list dictionary
+# nickname keys MUST be unique!
+contacts = {
+
+}
+
+
+#### TESTING THE FUNCTIONS
