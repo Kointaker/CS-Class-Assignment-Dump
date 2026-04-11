7ff258d69600cd2fb1b9aba0e2da04802819f900
Joshua Ferreira <kointaker@Joshuas-MacBook-Air.local>
2026-04-11 14:21:47 -0400

    Used boredAPI and implemented the get requests to fetch a random activity

--- api_practice/practice.py
+++ api_practice/practice.py
@@ -3,21 +3,18 @@ import requests
 
 
 # use requests.get() to get the data response from the API
-url = "https://api.weather.gov/"
+url = "https://bored-api.appbrewery.com/random"
 
 
 
-# 3. Make the request
-# If your API uses GET with query parameters:
-# response = requests.get(url, params=payload)
-# If it uses POST with JSON body (more common for optimizers):
+
 response = requests.get(url)
 
 # 4. Print the status code
 print(response.status_code)
 
 # 5. Print the JSON response
-print(response)
+print(response.json())
 
 
 
