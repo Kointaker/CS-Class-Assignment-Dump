f3bf26da1c0c4c0ddb3090fa4c7f51002828990b
Joshua Ferreira <kointaker@Joshuas-MacBook-Air.local>
2026-05-14 19:50:17 -0400

    Finished 9-7-----Added Administrator child class and it's implementation

--- OOP_Course/restaurant.py
+++ OOP_Course/restaurant.py
@@ -191,7 +191,20 @@ Wealth Class: {self.wealth}
 # privileges. Create an instance of Admin, and call your method.
 
 
+    class Administrator(User):
+        def __init__(self, first_name, last_name, id, ethnicity, wealth):
+            super().__init__(first_name, last_name, id, ethnicity, wealth)
 
+            self.privileges = ["Can add post", "Can delete post", "Can ban user", "Can spawn admin items", "Can use invincibility", "Can use admin sword"]
+
+
+        def show_privileges(self):
+            print(f"\nThe privileges that admin users have are: {self.privileges}")
+
+    Landon = Administrator("Landon", "Terry", 116644, "White", "Ultra Wealthy")
+    Landon.show_privileges()
+
+        
 
 
 
