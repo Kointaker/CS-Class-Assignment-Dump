4c66cd8057d0b37ddc57742d4842a5a83007266b
Joshua Ferreira <kointaker@Joshuas-MacBook-Air.local>
2026-05-16 10:24:48 -0400

    Added dice.py for 9-13, 9-14, and 9-15

---
+++ OOP_Course/dice.py
@@ -0,0 +1,26 @@
+# 9-13. Dice: Make a class Die with one attribute called sides, which has a
+# default value of 6. Write a method called roll_die() that prints a random num-
+# ber between 1 and the number of sides the die has. Make a 6-sided die and
+# roll it 10 times.
+# Make a 10-sided die and a 20-sided die. Roll each die 10 times.
+
+
+
+
+
+
+# 9-14. Lottery: Make a list or tuple containing a series of 10 numbers and 5 letters.
+# Randomly select 4 numbers or letters from the list and print a message saying that
+# any ticket matching these 4 numbers or letters wins a prize.
+
+
+
+
+
+
+
+
+# 9-15. Lottery Analysis: You can use a loop to see how hard it might be to win
+# the kind of lottery you just modeled. Make a list or tuple called my_ticket. Write
+# a loop that keeps pulling numbers until your ticket wins. Print a message report-
+# ing how many times the loop had to run to give you a winning ticket.
