5555e00c802b419d99f42dfc278d2a93849b00a7
Joshua Ferreira <kointaker@Joshuas-MacBook-Air.local>
2026-05-16 10:31:55 -0400

    Completed 9-13---Added Die class and it's implementation

--- OOP_Course/dice.py
+++ OOP_Course/dice.py
@@ -1,10 +1,24 @@
+import random
 # 9-13. Dice: Make a class Die with one attribute called sides, which has a
 # default value of 6. Write a method called roll_die() that prints a random num-
 # ber between 1 and the number of sides the die has. Make a 6-sided die and
 # roll it 10 times.
 # Make a 10-sided die and a 20-sided die. Roll each die 10 times.
+class Die:
+    def __init__(self, sides):
+        self.sides = sides
+    
+    def roll_die(self):
+        print(random.randint(1, 6))
 
+sided6 = Die(6)
+sided10 = Die(10)
+sided20 = Die(20)
 
+for i in range(10):
+    sided6.roll_die()
+    sided10.roll_die()
+    sided20.roll_die()
 
 
 
