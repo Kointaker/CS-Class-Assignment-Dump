f5265d834d9600d0d893f5073cd6df94546dd6e3
Joshua Ferreira <kointaker@Joshuas-MacBook-Air.local>
2026-05-16 09:53:42 -0400

    Changed modules.py to mine.py to avoid using the same name as an existing python module. Changed layout of mine.py codebase and remove the main() function to allow imports to work properly. Fixed minor issues in main.py

--- OOP_Course/main.py
+++ OOP_Course/main.py
@@ -1,10 +1,14 @@
+from mine import Restaurant
+
+
+
 #     9-10. Imported Restaurant: Using your latest Restaurant class, store it in a mod-
 # ule. Make a separate file that imports Restaurant. Make a Restaurant instance,
 # and call one of Restaurant’s methods to show that the import statement is work-
 # ing properly.
 
-
-
+new = Restaurant("Super-Foods", "American Food")
+print(new.number_served)
 
 
 
